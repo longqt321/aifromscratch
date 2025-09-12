c43f16d0ffaab7d823f54c19eeb927b412d747bd
lambdaliebert <longqt321@gmail.com>
2025-09-13 05:38:30 +0700

    added: graph, removed model from sklearn

--- examples/linear_regression.py
+++ examples/linear_regression.py
@@ -4,14 +4,14 @@ import math
 from sklearn.datasets import make_regression
 from sklearn.linear_model import LinearRegression
 from utils import train_test_split
+import matplotlib.pyplot as plt
 
 def mse(a,b):
     return np.mean(0.5 * (a-b)**2)
 
-X = np.random.rand(1000,1)
-y = 4*X + np.random.rand()
+X = np.random.rand(100,1)*3
+y = 4*X + 2 + np.random.rand(100,1)
 X_train,X_test,y_train,y_test = train_test_split(X,y)
-
 
 print(X.shape)
 print(y.shape)
@@ -23,12 +23,11 @@ y_pred = model.predict(X_test)
 # print(y_pred)
 print(mse(y_test,y_pred))
 
-print("BELOW IS GOOD MODEL!!")
-print(X_test.shape)
-model2 = LinearRegression(fit_intercept=False)
-model2.fit(X_train,y_train)
 
-y_pred = model2.predict(X_test)
-# print(y_pred)
-print(mse(y_test,y_pred))
 
+pX = np.array([[0],[3]])
+pY = model.predict(pX)
+
+plt.scatter(X,y)
+plt.plot(pX,pY,'red')
+plt.show()
--- supervised_learning/regression.py
+++ supervised_learning/regression.py
@@ -1,6 +1,15 @@
 import numpy as np
 import math
 
+class l1_regularization():
+    """ Regularization for Lasso Regression """
+    def __init__(self,alpha):
+        self.alpha = alpha
+    def __call__(self,w):
+        return self.alpha * np.linalg.norm(w)
+    def grad(self,w):
+        return self.alpha * self.sign(w)
+
 class Regression(object):
     def __init__(self, n_iters, lr):
         self.n_iters = n_iters
@@ -17,7 +26,7 @@ class Regression(object):
         for i in range(self.n_iters):
             y_pred = X.dot(self.w)
             
-            mse = np.mean(0.5 * (y-y_pred)**2)
+            mse = np.mean(0.5 * (y-y_pred)**2 + self.regularization(self.w)) 
             self.training_errors.append(mse)
 
             grad_w = -np.dot(X.T,(y-y_pred))
@@ -31,6 +40,10 @@ class Regression(object):
         return y_pred
 class LinearRegression(Regression):
     def __init__(self,n_iters=1000,lr=0.0001):
+
+        self.regularization = lambda x:0
+        self.regularization.grad = lambda x:0
+
         super(LinearRegression,self).__init__(n_iters=n_iters,lr=lr)
     def fit(self,X,y):
         super(LinearRegression,self).fit(X,y)
