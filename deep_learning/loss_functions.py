ad923e0c97c73d542f3a7fdacf48edffbc399ed5
lambdaliebert <longqt321@gmail.com>
2025-09-14 20:54:26 +0700

    added: loss functions

--- deep_learning/loss_functions.py
+++ deep_learning/loss_functions.py
@@ -22,10 +22,12 @@ class CrossEntropy(Loss):
     def __init__(self):
         pass
     def loss(self,y,p):
+        # Avoid division by zero
         p = np.clip(p,1e-15,1 - 1e-15)
         return -y*np.log(p) - (1-y)*np.log(1-p)
     def grad(self,y,p):
         p = np.clip(p,1e-15,1 - 1e-15)
         return -(y/p) + (1-y)/(1-p)
-    def acc(self,y,y_pred):
-        return 
+    def acc(self,y,p):
+        return accuracy_score(np.argmax(y,axis=1),np.argmax(p,axis=1))
+    
